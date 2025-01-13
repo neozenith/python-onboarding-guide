5c2f568893d557fd28003488e6e27d14305c8cf3
Josh Peak <joshpeak05@gmail.com>
2025-01-13 12:16:01 +1100

    Update clean_slice.sql

--- dbt-project/macros/clean_slice.sql
+++ dbt-project/macros/clean_slice.sql
@@ -1,30 +1,37 @@
 {% macro clean_slice(debug_logging=false) -%}
 
+  {#- we replace characters not allowed in the schema names by "_" -#}
+  {%- set re = modules.re -%}
         
+  {#- https://docs.getdbt.com/docs/build/environment-variables#special-environment-variables -#} 
+  {%- set pr_id = re.sub("\W", "_", env_var('DBT_CLOUD_PR_ID', '')) | trim | upper -%}
+  {%- set job_id = re.sub("\W", "_", env_var('DBT_CLOUD_JOB_ID', '')) | trim | upper -%}
+  {%- set run_id = re.sub("\W", "_", env_var('DBT_CLOUD_RUN_ID', '')) | trim | upper -%}
 
-        {#- we replace characters not allowed in the schema names by "_" -#}
-        {%- set re = modules.re -%}
+  {#- DEV environment uses branch -#}
+  {%- set branch_slice = re.sub("\W", "_", 
+      env_var('DBT_CLOUD_GIT_BRANCH', 
+        env_var('DBT_GIT_BRANCH', 
+          env_var('GIT_BRANCH', 
+            var('git_branch')
+          )
+        )
+      )
+    )
+   -%}
         
-        {#- TODO: Include special environment variables as part of the naming conventions for a slice.https://docs.getdbt.com/docs/build/environment-variables#special-environment-variables -#} 
-        {%- set pr_id = re.sub("\W", "_", env_var('DBT_CLOUD_PR_ID', '')) | trim | upper -%}
-        {%- set job_id = re.sub("\W", "_", env_var('DBT_CLOUD_JOB_ID', '')) | trim | upper -%}
-        {%- set run_id = re.sub("\W", "_", env_var('DBT_CLOUD_RUN_ID', '')) | trim | upper -%}
+  {#- TEST Environment uses CI values -#}
+  {%- set ci_slice = "PR" ~ pr_id ~ "_JOB" ~ job_id ~ "_RUN" ~ run_id -%}
 
-        {#- DEV environment uses branch -#}
-        {%- set branch_slice = re.sub("\W", "_", env_var('DBT_CLOUD_GIT_BRANCH', env_var('DBT_GIT_BRANCH', var('git_branch')))) -%}
-        
-        {#- TEST Environment uses CI values -#}
-        {%- set ci_slice = "PR" ~ pr_id ~ "_JOB" ~ job_id ~ "_RUN" ~ run_id -%}
+  {%- set cleaned_slice = "" -%}        
 
-        {% if branch_slice is not none and branch_slice | length > 0 %}
-                {%- set cleaned_slice = branch_slice -%}
-        {% else %}
-                {%- set cleaned_slice = ci_slice -%}        
-        {% endif %}
+  {% if branch_slice is not none and branch_slice | length > 0 %}
+    {%- set cleaned_slice = branch_slice -%}
+  {% elif ci_slice is not none and ci_slice | length > 10 %}
+    {%- set cleaned_slice = ci_slice -%}        
+  {% endif %}
 
-
-        {% do log("Clean slice: " ~ cleaned_slice, info=debug_logging) %}
-        {{- cleaned_slice -}}
-        
+  {% do log("Clean slice: " ~ cleaned_slice, info=debug_logging) %}
+  {{- cleaned_slice | trim -}}       
 
 {% endmacro %}
